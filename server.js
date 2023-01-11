59da4b75f2be238a6be22e82fdff88c15b0e4d31
Calvin Chern <calvin.chern24@gmail.com>
2023-01-11 18:45:53 -0500

    modification of server.js

--- server.js
+++ server.js
@@ -1,21 +1,29 @@
 const fs = require('fs');
 const express = require('express');
 const path = require('path');
-const dbdata = require('./db/db.json')
-const app = express();
+const { notes }= require('./db/db.json')
+const api = require('./routes/index.js');
+
 const PORT = process.env.PORT || 3001;
+
+const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
   });
 
-app.get('/api/terms', (req, res) => res.json(dbdata));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/notes.html'));
+  });
+
+app.get('/api/notes', (req, res) => res.json(notes));
 
 app.listen(PORT, () => {
-    console.log(`Note taker will be at http://localhost:${PORT}`);
+    console.log(`App listening at http://localhost:${PORT}`);
   });
+
+app.post()
